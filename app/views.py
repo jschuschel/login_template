6e408003dcb6b1743b7a2c2c99b86d8bb885b020
jschuschel <jschuschel@llnw.com>
2015-07-27 14:31:37 -0700

    — Checkpoint: 07_form_validations —
    http://code.tutsplus.com/tutorials/intro-to-flask-adding-a-contact-page--net-28982

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 __author__ = 'jschuschel'
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from forms import ContactForm
 
 app = Flask(__name__)
-
+app.secret_key = "Dis be my key right hur"
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -10,6 +11,17 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+    form = ContactForm()
+
+    if request.method == 'POST':
+        return 'Form Posted.'
+    elif request.method == 'GET':
+        return render_template('contact.html', form=form)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)

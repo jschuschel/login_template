f23c416b4b679a4b7f18f49e94db53d3aee0dd7a
jschuschel <jschuschel@llnw.com>
2015-07-27 14:52:49 -0700

    — Checkpoint: 12_contact_nav_link — http://code.tutsplus.com/tutorials/intro-to-flask-adding-a-contact-page--net-28982
    next section

--- app/views.py
+++ app/views.py
@@ -1,9 +1,23 @@
 __author__ = 'jschuschel'
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+from flask.ext.mail import Message, Mail
 from forms import ContactForm
 
+#app settings
 app = Flask(__name__)
 app.secret_key = "Dis be my key right hur"
+
+mail = Mail()
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = 'contact@example.com'
+app.config["MAIL_PASSWORD"] = 'your-password'
+
+mail.init_app(app)
+
+#app views
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -17,7 +31,18 @@ def contact():
     form = ContactForm()
 
     if request.method == 'POST':
-        return 'Form Posted.'
+        if form.validate() == False:
+            flash('All fields are required.')
+            return render_template('contact.html', form=form)
+        else:
+            msg = Message(form.subject.data, sender='contact@example.com', recipients=['your_email@example.com'])
+            msg.body = """
+            From: %s <%s>
+            %s
+            """ % (form.name.data, form.email.data, form.message.data)
+            mail.send(msg)
+
+            return render_template('contact.html', success=True)
     elif request.method == 'GET':
         return render_template('contact.html', form=form)
 
